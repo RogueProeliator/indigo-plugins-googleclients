517cf750b437b23071391643bece84c1b232cbe0
RogueProeliator <rp@rogueproeliator.com>
2023-10-31 22:00:58 -0500

    Removed Google Home references from plugin

--- src/DomoPad Mobile Client Plugin.indigoPlugin/Contents/Server Plugin/plugin.py
+++ src/DomoPad Mobile Client Plugin.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -12,7 +12,6 @@ import requests
 
 import RPFramework
 import domoPadDevices
-import googleHomeDevices
 import dicttoxml
 
 from RPFramework.RPFrameworkPlugin import RPFrameworkPlugin
@@ -20,16 +19,10 @@ from RPFramework.RPFrameworkCommand import RPFrameworkCommand
 # endregion
 
 # region Constants and configuration variables
-DOMOPADCOMMAND_SENDNOTIFICATION                = 'SendNotification'
-DOMOPADCOMMAND_SPEAKANNOUNCEMENTNOTIFICATION   = 'SendTextToSpeechNotification'
-DOMOPADCOMMAND_CPDISPLAYNOTIFICATION           = 'SendCPDisplayRequest'
-DOMOPADCOMMAND_DEVICEUPDATEREQUESTNOTIFICATION = 'RequestDeviceStatusUpdate'
-
-GOOGLEHOME_SENDDEVICEUPDATE = 'SendHomeGraphUpdate'
-GOOGLEHOME_REQUESTSYNC      = 'RequestHomeGraphSync'
-
-INDIGO_SERVER_CLOUD_URL     = 'https://us-central1-domotics-pad-indigo-client.cloudfunctions.net/indigo-server-portal'
-
+DOMOPADCOMMAND_SENDNOTIFICATION                = "SendNotification"
+DOMOPADCOMMAND_SPEAKANNOUNCEMENTNOTIFICATION   = "SendTextToSpeechNotification"
+DOMOPADCOMMAND_CPDISPLAYNOTIFICATION           = "SendCPDisplayRequest"
+DOMOPADCOMMAND_DEVICEUPDATEREQUESTNOTIFICATION = "RequestDeviceStatusUpdate"
 # endregion
 
 
@@ -65,10 +58,6 @@ class Plugin(RPFrameworkPlugin):
 	# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 	def startup(self):
 		super(Plugin, self).startup()
-		
-		# subscribe to all devices changes so that we may push them up to Google Home
-		# (if so configured)
-		indigo.devices.subscribeToChanges()
 
 	# endregion
 	#######################################################################################
@@ -80,26 +69,7 @@ class Plugin(RPFrameworkPlugin):
 	# can/should be overridden in the plugin implementation (if needed)
 	# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 	def handle_unknown_plugin_command(self, rp_command, requeue_commands_list):
-		if rp_command.command_name == GOOGLEHOME_SENDDEVICEUPDATE:
-			try:
-				reflector_url     = f"{indigo.server.getReflectorURL()}/"
-				device_update_xml = dicttoxml.dicttoxml(rp_command.command_payload, True, "Device")
-				request_body      = {"intent": "googlehomegraph.UPDATE_DEVICE", "payload": {"agentId": reflector_url, "deviceUpdate": device_update_xml}}
-				self.logger.info(f"Sending {json.dumps(request_body)}")
-				requests.post(INDIGO_SERVER_CLOUD_URL, data=json.dumps(request_body))
-			except:
-				self.logger.exception("Failed to send device update to Google Home")
-
-		elif rp_command.command_name == GOOGLEHOME_REQUESTSYNC:
-			try:
-				reflector_url = f"{indigo.server.getReflectorURL()}/'"
-				request_body  = f'{{ "intent": "googlehomegraph.REQUEST_SYNC", "payload": {{ "agentId": "{reflector_url}" }} }}'
-				self.logger.debug(f"Sending intent to Indigo Cloud for synchronization with {reflector_url}")
-				requests.post(INDIGO_SERVER_CLOUD_URL, data=request_body)
-			except:
-				self.logger.exception('Failed to request that device definitions re-synchronize with Google Home/Assistant')
-
-		elif rp_command.command_name == DOMOPADCOMMAND_SENDNOTIFICATION:
+		if rp_command.command_name == DOMOPADCOMMAND_SENDNOTIFICATION:
 			self.logger.threaddebug(f"Push Notification Send Command: DevicePairID={rp_command.command_payload[0]}; Type={rp_command.command_payload[2]}; Message={rp_command.command_payload[1]}")
 
 			# set up the defaults so that we know all the parameters have a value...
@@ -267,130 +237,7 @@ class Plugin(RPFrameworkPlugin):
 	#######################################################################################
 
 	#######################################################################################
-	# region Plugin Event Overrides
-	# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-	# Called whenever a device updates... if it is one of the monitored devices then
-	# send the updates to Google Home
-	# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-	def deviceUpdated(self, origDev, newDev):
-		self.logger.debug(f"Received device update for {newDev.name}")
-
-		# call the base's implementation first just to make sure all the right things happen elsewhere
-		indigo.PluginBase.deviceUpdated(self, origDev, newDev)
-
-		# we only care about devices which are published to Google Home and only whenever
-		# the option to send devices changes is checked
-		if self.report_state_to_assistant and "com.indigodomo.indigoserver" in newDev.globalProps:
-			global_props_dict = newDev.globalProps["com.indigodomo.indigoserver"]
-			if global_props_dict.get("googleClientPublishHome", False) and global_props_dict.get("googleClientSendUpdates", False):
-				try:
-					# retrieve the device update from the server in the same format as the query for
-					# the device status
-					device_update = indigo.rawServerRequest("GetDevice", {"ID": newDev.id})
-
-					# schedule a call to the Google Home Graph's update via the Cloud Function
-					self.logger.info(f"Scheduling device update of {newDev.id} ({newDev.name}) with Google Home/Assistant")
-					self.logger.debug(f"Sending device update: {dicttoxml.dicttoxml(device_update, True, 'Device')}")
-					self.plugin_command_queue.put(RPFrameworkCommand(GOOGLEHOME_SENDDEVICEUPDATE, device_update))
-				except:
-					self.logger.exception(f"Failed to generate Google Home update for device {newDev.name}")
-
-	# endregion
-	#######################################################################################
-
-	#######################################################################################
 	# region Configuration Dialog Callback Routines
-	# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-	# Retrieves the list of all indigo devices which may be published to the Google Home
-	# application; sorted by its published status
-	# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-	def get_google_home_device_list(self, filter="", values_dict=None, type_id="", target_id=0):
-		published_home_devices = []
-		available_home_devices = []
-
-		for dev in indigo.devices:
-			if dev.sharedProps.get('googleClientPublishHome', False):
-				published_home_devices.append((dev.id, dev.name))
-			else:
-				available_home_devices.append((dev.id, dev.name))
-
-		# build the complete list of devices
-		return_list = [(-1, "%%disabled:Devices available for publishing%%")]
-		return_list.extend(available_home_devices)
-
-		return_list.append((-2, "%%separator%%"))
-		return_list.append((-3, "%%disabled:Devices already published to Google Assistant%%"))
-		return_list.extend(published_home_devices)
-		return return_list
-
-	# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-	# Retrieves the list of available (supported) devices types as defined by the
-	# Google Assistant
-	# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-	def get_google_device_types(self, filter="", values_dict=None, type_id="", target_id=0):
-		if values_dict is None:
-			return []
-			
-		published_device_id = f'{values_dict.get("publishedDevice", "")}'
-		if published_device_id is None or published_device_id == "":
-			return []
-
-		device = indigo.devices.get(int(published_device_id), None)
-		if device is None:
-			return []
-		else:
-			return googleHomeDevices.get_subtypes_for_device(device)
-
-	# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-	# Called whenever the user has selected a device from the list of published Google
-	# Assistant devices... show the "Google" device details
-	# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-	def published_home_device_selected(self, values_dict=None, type_id="", dev_id=0):
-		try:
-			device = indigo.devices.get(int(values_dict["publishedDevice"]), None)
-
-			values_dict["enableDeviceDetailUI"]       = True
-			values_dict["publishToGoogle"]            = device.sharedProps.get("googleClientPublishHome", False)
-			values_dict["deviceDetailsPublishedName"] = device.sharedProps.get("googleClientAsstName"   , "")
-			values_dict["deviceDetailsPublishedType"] = device.sharedProps.get("googleClientAsstType"   , "")
-			values_dict["sendUpdatesToGoogle"]        = device.sharedProps.get("googleClientSendUpdates", "")
-			values_dict["deviceDetailsPINCode"]       = device.sharedProps.get("googleClientPINCode"    , "")
-		except:
-			self.logger.exception("Failed to load published device properties")
-		return values_dict
-
-	# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-	# Called whenever the user has clicked to update the published Google Home (such as
-	# changing the device name or type)
-	# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-	def published_home_devices_update(self, values_dict=None, type_id="", dev_id=0):
-		try:
-			device      = indigo.devices.get(int(values_dict["publishedDevice"]), None)
-			global_props = device.sharedProps
-
-			global_props["googleClientPublishHome"] = values_dict["publishToGoogle"]
-			global_props["googleClientAsstName"]    = values_dict["deviceDetailsPublishedName"]
-			global_props["googleClientAsstType"]    = values_dict["deviceDetailsPublishedType"]
-			global_props["googleClientSendUpdates"] = values_dict["sendUpdatesToGoogle"]
-			global_props["googleClientPINCode"]     = values_dict["deviceDetailsPINCode"]
-			device.replaceSharedPropsOnServer(global_props)
-
-			values_dict["publishedDevice"]            = None
-			values_dict["publishToGoogle"]            = False
-			values_dict["publishedDeviceSelected"]    = False
-			values_dict["deviceDetailsPublishedName"] = ""
-			values_dict["deviceDetailsPublishedType"] = None
-			values_dict["sendUpdatesToGoogle"]        = False
-			values_dict["deviceDetailsPINCode"]       = ""
-
-			values_dict["enableDeviceDetailUI"]       = False
-
-			# let Google know that a synchronization is required
-			
-		except:
-			self.logger.exception("Failed to update published device properties")
-		return values_dict
-
 	# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 	# When called, clears the current device pairing ID, disabling push notification and
 	# updates from the old device
@@ -399,62 +246,11 @@ class Plugin(RPFrameworkPlugin):
 		values_dict["deviceRegistrationId"] = ""
 		return values_dict
 
-	# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-	# Processes the user requesting that the Google Home Graph re-sync the set of
-	# published devices
-	# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-	def request_resync_with_google(self):
-		# simply schedule a resynchronization request with the background processor
-		self.logger.info("Scheduling re-synchronization request with Google Home/Assistant")
-		self.plugin_command_queue.put(RPFrameworkCommand(GOOGLEHOME_REQUESTSYNC))
-
 	# endregion
 	#######################################################################################
 
 	#######################################################################################
 	# region API Action Handlers
-	# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-	# Handles an API from the Google Assistant Action requesting synchronization of
-	# all devices
-	# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-	def google_request_sync_devices(self, action, dev=None, caller_waiting_for_result=None):
-		published_home_devices = []
-		for dev in indigo.devices:
-			if dev.sharedProps.get("googleClientPublishHome", False):
-				published_home_devices.append(dev)
-
-		response_payload = googleHomeDevices.build_google_homegraph_sync_response(published_home_devices)
-		return json.dumps(response_payload)
-
-	# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-	# Sets the Setpoint of a thermostat using the current thermostat mode (heating or
-	# cooling)
-	# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-	def set_thermostat_setpoint(self, action, dev=None, caller_waiting_for_result=None):
-		try:
-			device_id    = int(action.props["body_params"].get("deviceId", "0"))
-			new_setpoint = float(action.props["body_params"].get("setpoint", "0.0"))
-			is_success   = False
-			message      = ""
-
-			if device_id == 0 or new_setpoint == 0.0:
-				self.logger.warning("Unable to process API request to set thermostat setpoint due to missing or invalid arguments")
-				message = "Unable to process API request to set thermostat setpoint due to missing or invalid arguments"
-			else:
-				device = indigo.devices[device_id]
-				if device.hvacMode == indigo.kHvacMode.HeatCool or device.hvacMode == indigo.kHvacMode.ProgramHeatCool or device.hvacMode == indigo.kHvacMode.Cool or device.hvacMode == indigo.kHvacMode.ProgramCool:
-					indigo.thermostat.setCoolSetpoint(device_id, value=new_setpoint)
-					is_success = True
-				elif device.hvacMode == indigo.kHvacMode.Heat or device.hvacMode == indigo.kHvacMode.ProgramHeat:
-					indigo.thermostat.setHeatSetpoint(device_id, value=new_setpoint)
-					is_success = True
-				else:
-					message = "Thermostat is off or not in a mode that accepts set point changes"
-
-			return f'{{ "result": "{is_success}", "message": "{message}" }}'
-		except Exception as ex:
-			self.logger.exception(f"Failed to set thermostat set point via API: {ex}")
-
 	# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 	# API call that allows the Android client to register itself against a specific Indigo
 	# Android device
